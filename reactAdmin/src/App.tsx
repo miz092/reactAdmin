17ad31696f28fa6e5c3b0b6d790f90dd160c5138
Zoltán Mihályfi <mihalyfi.zoltan@gmail.com>
2024-02-07 13:02:27 +0100

    init project

---
+++ reactAdmin/src/App.tsx
@@ -0,0 +1,15 @@
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  EditGuesser,
+  ShowGuesser,
+} from "react-admin";
+import { dataProvider } from "./dataProvider";
+
+export const App = () => <Admin dataProvider={dataProvider}>
+  <Resource name="todos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+  <Resource name="comments" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+  <Resource name="users" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+
+</Admin>;
